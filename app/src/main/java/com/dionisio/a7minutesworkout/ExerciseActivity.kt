1db7388750a80fd65a2cd04c603a69549e65efa0
Dionisio <dionisio@malteso.com>
2022-11-01 13:59:49 -0400

    New Screen and Button Ready

---
+++ app/src/main/java/com/dionisio/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,25 @@
+package com.dionisio.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dionisio.a7minutesworkout.databinding.ActivityExerciseBinding
+import com.dionisio.a7minutesworkout.databinding.ActivityMainBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding : ActivityExerciseBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/dionisio/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/dionisio/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dionisio.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         binding?.flStart?.setOnClickListener {
             Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
 
